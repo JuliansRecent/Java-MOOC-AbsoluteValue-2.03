7dba317df3ec06569060e8677c369e93ce865c97
JuliansRecent <juliansrecent@gmail.com>
2023-02-19 13:05:56 -0800

    Module complete. Create a terminal based application that takes in an integer input and outputs the aboslute value of the integer.

--- src/main/java/AbsoluteValue.java
+++ src/main/java/AbsoluteValue.java
@@ -6,5 +6,16 @@ public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Collect the inputted number from the user
+        int input = Integer.valueOf(scanner.nextLine());
+
+        // If the number is less than 0, multiply the number by -1 and print the output
+        if (input < 0) {
+            System.out.println(input * -1);
+        } else {
+            // Else, just print the inputted number
+            System.out.println(input);
+        }
+
     }
 }
